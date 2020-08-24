2cb860f7945cb6cb26bfa8a04d51e7a5fdd1fb41
Aline <alinesanqueza@gmail.com>
2020-08-24 19:04:07 -0300

    banco de dados

---
+++ api/server/src/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "burger_queen_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "burger_queen_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "burger_queen_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
